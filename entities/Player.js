f56a3a7617b8971706c7d4edd65e31d498eb2a2c
JMFlawy <jyjmar@hotmail.com>
2026-08-04 00:00:21 +0200

    Update Player.js

--- entities/Player.js
+++ entities/Player.js
@@ -8,7 +8,7 @@ export default class Player {
     this.width = 72;
     this.height = 108;
 const isMobile = /Android|iPhone|iPad|iPod|Mobile/i.test(navigator.userAgent);
-    this.speed = isMobile ? 3.0 : 1.8;
+    this.speed = isMobile ? 3.4 : 1.8;
 
     this.vx = 0;
     this.vy = 0;
