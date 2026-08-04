1009e6d5ff7b4412a4c1565d9e4d281af1f7bcf9
JMFlawy <jyjmar@hotmail.com>
2026-08-04 02:15:16 +0200

    Update Game.js

--- engine/Game.js
+++ engine/Game.js
@@ -155,7 +155,6 @@ this.truckVideo = document.createElement("video");
 this.truckVideo.src = "assets/contenedores/video.mp4";
 this.truckVideo.preload = "auto";
 this.truckVideo.playsInline = true;
-this.truckVideo.muted = true;
 
 this.truckVideoPlaying = false;
 this.truckVideoFinished = false;
