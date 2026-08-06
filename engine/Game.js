6e9290582f937f012787f774b9dbc51bbe9a9cd1
JMFlawy <jyjmar@hotmail.com>
2026-08-06 02:32:16 +0200

    Adjust truck movement speed for non-mobile devices

--- engine/Game.js
+++ engine/Game.js
@@ -366,7 +366,7 @@ export default class Game {
 
   updateTruckSequence() {
     const isMobile = /Android|iPhone|iPad|iPod|Mobile/i.test(navigator.userAgent);
-    const speed = isMobile ? 1.8 : 0.6;
+    const speed = isMobile ? 1.8 : 0.7;
 
     if (!this.truckLeaving && !this.truckVideoPlaying && !this.truckVideoFinished) {
       if (this.truckX > this.truckTargetX) {
